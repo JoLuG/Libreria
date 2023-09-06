edd3f0b4cf32064b9b8994f37fd5e06848c8b218
JoLuG <jorgonzsu025@outlook.com>
2023-09-05 23:45:59 -0600

    Ya esta!!

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\LibroController;
-
+use App\Http\Controllers\Api\CuponAplicableController;
+use App\Http\Controllers\Api\ResenaController;
+use App\Http\Controllers\Api\CategoriaController;
+use App\Http\Controllers\Api\ClienteController;
+use App\Http\Controllers\Api\CompraController;
+use App\Http\Controllers\Api\CompraLibroController;
+use App\Http\Controllers\Api\DetallePedidoController;
+use App\Http\Controllers\Api\PedidoController;
+use App\Http\Controllers\Api\MultimediaController;
+use App\Http\Controllers\Api\IdiomaController;
+use App\Http\Controllers\Api\DimensionController;
+use App\Http\Controllers\Api\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +27,17 @@ use App\Http\Controllers\Api\LibroController;
 |
 */
 Route::apiResource('libros', LibroController::class);
+Route::apiResource('cupones_aplicables', CuponAplicableController::class);
+Route::apiResource('resenas', ResenaController::class);
+Route::apiResource('categorias', CategoriaController::class);
+Route::apiResource('clientes', ClienteController::class);
+Route::apiResource('compras', CompraController::class);
+Route::apiResource('detalle_pedidos', DetallePedidoController::class);
+Route::apiResource('pedidos', PedidoController::class);
+Route::apiResource('multimedias', MultimediaController::class);
+Route::apiResource('idiomas', IdiomaController::class);
+Route::apiResource('dimensiones', DimensionController::class);
+Route::apiResource('users', UserController::class);
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
